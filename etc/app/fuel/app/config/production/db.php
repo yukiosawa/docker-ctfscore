c2e0eeca6cfb66bfe29fc5d6a491889853144063
Yuki <yukiosawa@users.noreply.github.com>
2015-09-04 04:46:58 +0900

    fuelphp production環境設定

---
+++ etc/app/fuel/app/config/production/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The production database settings. These get merged with the global settings.
+ */
+
+return array(
+    'default' => array(
+	'type' => 'mysqli',
+	'connection'  => array(
+	    /* 'dsn'        => 'mysql:host=localhost;dbname=ctf_score', */
+	    'hostname'   => 'localhost',
+	    'port'       => '3306',
+	    'database'   => 'ctf_score',
+	    'username'   => 'admin',
+	    'password'   => 'mypasswd',
+	),
+    ),
+);
+
